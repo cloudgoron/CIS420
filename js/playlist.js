64411e222af6595011267b66ac6bf917f7ac9202
Bill Jordan <wiljor7314@students.ecpi.edu>
2015-02-18 18:12:00 -0500

    vehicle.html is working

--- js/playlist.js
+++ js/playlist.js
@@ -2,21 +2,70 @@
 This is a test list that will be replaced with vehicle, driver, etc lists for the carosels
 */
 
-var cars = [
+var limos = [
 	
 	{
-		img: "images/images.jpeg",
-		text: "this is the first car"
+		img: "images/limo_1.jpeg",
+		txt: "this is the first limo",
 	},
 
 	{
-		img: "images/images-2.jpeg",
-		text: "this is the second car"
-	}
+		img: "images/limo_2.jpeg",
+		txt: "this is the second limo"
+
+	},
+
+	{
+		img: "images/limo_3.jpeg",
+		txt: "this is the third limo"
+
+	},
 ];
 
-function printCars(){
-	for(var i = 0; i < 2; i ++){
-		document.writeln(cars[i]["img"]);
+var suvs = [
+	
+	{
+		img: "images/suv_1.jpeg",
+		txt: "this is the first suv",
+	},
+
+	{
+		img: "images/suv_2.jpeg",
+		txt: "this is the second suv"
+
+	},
+
+	{
+		img: "images/suv_3.jpeg",
+		txt: "this is the third suv"
+
+	},
+];
+
+var sedans = [
+	{
+		img: "images/sedan_1.jpeg",
+		txt: "this is the first car",
+	},
+
+	{
+		img: "images/sedan_2.jpeg",
+		txt: "this is the second car"
+
+	},
+]
+
+function printListAsCarousel(list, carType){
+	//this function assumes the list is not empty
+	var img = list[0].img;
+	var txt = list[0].txt;
+	var htmlStr = '<div class="item active"><img src="' + img + '" alt="..."><div class="carousel-caption"><p>'+txt+'<br/><form action="route.html" method = "get"><input type="hidden" name="carType" value="'+carType+'"><input type="hidden" name="car" value="0"><input type="submit" value = "I want this one"></form></p></div></div>';
+	// document.writeln(cars.length);
+	for(var i = 1; i < list.length; i ++){
+		img = list[i].img;
+		txt = list[i].txt;
+		htmlStr += '<div class="item"><img src="' + img + '" alt="..."><div class="carousel-caption"><p>'+txt+'<form action="route.html" method = "get"><input type="hidden" name="carType" value="'+carType+'"><input type="hidden" name="car" value="'+i+'"><input type="submit" value = "I want this one"></form></p></div></div>';
 	}
-}
+	return htmlStr;
+};
+
